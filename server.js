2b3be57e8c85aceb44e589678f77413225742825
gilbertboys <gilbertboys>
2026-06-18 17:28:18 -0700

    Fix Heroku PORT binding

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const exphbs = require('express-handlebars');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
